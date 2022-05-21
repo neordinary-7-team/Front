5aed3551889aee55685015fb55dd9a1b7b693b3a
min <hororonoodle@gmail.com>
2022-05-22 06:24:50 +0900

    feat. : 줄 제거

--- team-7/src/components/ExpoCalendar.tsx
+++ team-7/src/components/ExpoCalendar.tsx
@@ -98,50 +98,19 @@ export default function App() {
       ) : (
         <View onTouchStart={() => Alert.alert('일정을 먼저 생성해주세요.')}>
           <Calendar
-            // Initially visible month. Default = now
             current={moment().format('YYYY-MM-DD').toString()}
-            // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-            minDate={'2012-05-10'}
-            // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-            maxDate={'2012-05-30'}
-            // Handler which gets executed on day press. Default = undefined
-
-            // Handler which gets executed on day long press. Default = undefined
-            onDayLongPress={(day) => {
-              console.log('selected day', day);
-            }}
-            // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
+            minDate={'2000-05-10'}
+            maxDate={'2000-05-30'}
             monthFormat={'yyyy MM'}
-            // Handler which gets executed when visible month changes in calendar. Default = undefined
-
-            // Hide month navigation arrows. Default = false
             hideArrows={true}
-            // Replace default arrows with custom ones (direction can be 'left' or 'right')
-            renderArrow={(direction) => <Arrow />}
-            // Do not show days of other months in month page. Default = false
             hideExtraDays={true}
-            // If hideArrows = false and hideExtraDays = false do not switch month when tapping on greyed out
-            // day from another month that is visible in calendar page. Default = false
             disableMonthChange={true}
-            // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday
             firstDay={1}
-            // Hide day names. Default = false
             hideDayNames={true}
-            // Show week numbers to the left. Default = false
             showWeekNumbers={true}
-            // Handler which gets executed when press arrow icon left. It receive a callback can go back month
-            onPressArrowLeft={(subtractMonth) => subtractMonth()}
-            // Handler which gets executed when press arrow icon right. It receive a callback can go next month
-            onPressArrowRight={(addMonth) => addMonth()}
-            // Disable left arrow. Default = false
             disableArrowLeft={true}
-            // Disable right arrow. Default = false
             disableArrowRight={true}
-            // Disable all touch events for disabled days. can be override with disableTouchEvent in markedDates
             disableAllTouchEventsForDisabledDays={true}
-            // Replace default month and year title with custom one. the function receive a date as parameter
-
-            // Enable the option to swipe between months. Default = false
             enableSwipeMonths={true}
           />
         </View>
