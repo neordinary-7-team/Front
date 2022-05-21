537e80e4c3f4d87e7c54efb7b030cd74f3cfe11e
min <hororonoodle@gmail.com>
2022-05-22 05:03:35 +0900

    feat : api 연결

--- team-7/src/components/ExpoCalendar.tsx
+++ team-7/src/components/ExpoCalendar.tsx
@@ -3,13 +3,16 @@ import { StyleSheet, Pressable } from 'react-native';
 import { Calendar, DateData } from 'react-native-calendars';
 import { View, Text, TextInput } from '../theme';
 import moment from 'moment';
+import axios from 'axios';
 import { Colors } from 'react-native-paper';
 
 export default function App() {
-  const [selectedDate, setSelectedDate] = useState<String[]>([
+  const [selectedDate, setSelectedDate] = useState<string[]>([
     moment().format('YYYY-MM-DD').toString(),
   ]);
+  const [text, setText] = useState('');
   const [markedDates, setMarkedDates] = useState({});
+
   const handleDateClick = (day: DateData) => {
     if (selectedDate.includes(day.dateString)) {
       setSelectedDate(selectedDate.filter((dates) => dates !== day.dateString));
@@ -17,16 +20,32 @@ export default function App() {
       setSelectedDate((dates) => [...dates, day.dateString]);
     }
   };
+
   useEffect(() => {
-    let obj = selectedDate.reduce(
+    const obj = selectedDate.reduce(
       (c, v) =>
         Object.assign(c, {
           [v.toString()]: { selected: true },
         }),
-      {}
+      {},
     );
     setMarkedDates(obj);
   }, [selectedDate]);
+
+  const handleMakeSchedule = () => {
+    axios
+      .post('http://15.165.67.130:9000/schedules', {
+        groupName: text,
+        userIdx: 1,
+        dateList: selectedDate,
+      })
+      .then(function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
   return (
     <View style={styles.container}>
@@ -35,8 +54,17 @@ export default function App() {
           style={styles.textInput}
           placeholder="그룹명을 입력해주세요"
           placeholderTextColor={'gray'}
+          onChangeText={(text) => setText(text)}
         />
-        <Pressable style={styles.btn}>
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? Colors.lightBlue600 : Colors.lightBlue500,
+            },
+            styles.btn,
+          ]}
+          onPress={handleMakeSchedule}
+        >
           <Text style={styles.text}>일정 생성하기</Text>
         </Pressable>
       </View>
@@ -47,11 +75,28 @@ export default function App() {
           <Text key={idx}>{val}</Text>
         ))}
       </View> */}
+      <View style={styles.flex} />
       <View style={styles.btnView}>
-        <Pressable style={[styles.btn]} onPress={() => setSelectedDate([])}>
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? Colors.lightBlue600 : Colors.lightBlue500,
+            },
+            styles.btn,
+          ]}
+          onPress={() => setSelectedDate([])}
+        >
           <Text style={styles.text}>선택된 날짜 모두 지우기</Text>
         </Pressable>
-        <Pressable style={styles.btn} onPress={() => setSelectedDate([])}>
+        <Pressable
+          style={({ pressed }) => [
+            {
+              backgroundColor: pressed ? Colors.lightBlue600 : Colors.lightBlue500,
+            },
+            styles.btn,
+          ]}
+          onPress={() => setSelectedDate([])}
+        >
           <Text style={styles.text}>일정 공유하기</Text>
         </Pressable>
       </View>
@@ -62,6 +107,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'column',
+    flex: 1,
+  },
+  flex: {
     flex: 1,
   },
   dateView: {
@@ -80,7 +128,6 @@ const styles = StyleSheet.create({
     padding: 10,
     marginTop: 10,
     borderRadius: 10,
-    backgroundColor: Colors.lightBlue500,
   },
   textInputView: {
     padding: 10,
